bf1b5aa86a203aa6a94a1beec48a1c762e1f7e90
shivam uttpal jha <shivamuttpal@gmail.com>
2022-05-06 00:02:07 +0530

    set matrix zero

---
+++ removeduplicatefromsortedarray.cpp
@@ -0,0 +1,20 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+//  int removeDuplicates(vector<int>& nums) {
+//         set<int> s;
+//         for(int i=0;i<nums.size();i++){
+//             s.insert(nums[i]);
+//         }
+//         int k=s.size();
+//         return k;
+// }
+
+// int main()
+// {
+
+//     vector<int>v={0,0,1,1,1,2,2,3,3,4};
+
+//     cout<<removeDuplicates(v);
+//     return 0;
+// }
